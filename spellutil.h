13d6f51d99675d5c31d4fb6c6fc18dcb1acb9ba0
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2014-05-25 16:57:25 +0530

    Add interface for a hash table implementation required for the next stages of the spell checker implementation.

--- spellutil.h
+++ spellutil.h
@@ -6,8 +6,19 @@ typedef struct spell_list_node {
     void *data;
 } spell_list_node;
 
+typedef struct spell_hashtable {
+    char *key;
+    spell_list_node *val;
+} spell_hashtable;
+
 spell_list_node *spell_list_init(void *);
 int spell_list_add(spell_list_node **, void *);
+void spell_list_remove(spell_list_node **, spell_list_node *);
 void spell_list_free(spell_list_node **, void (*) (void *));
+spell_hashtable *spell_hashtable_init(size_t);
+void spell_hashtable_add(spell_hashtable *, char *, void *);
+void spell_hashtable_remove(spell_hashtable *, char *);
+void spell_hashtable_get(spell_hashtable *, char *);
+void spell_hashtable_free(spell_hashtable *, void (*) (void *));
 
 #endif
