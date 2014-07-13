9bcea2bcaeb11e4ed16a0030aa1b363b06343a7c
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2014-07-14 02:12:36 +0530

    Use getdelim instead of getline as the ultimate goal is to read words
    Add a main function with hardcoded file names to test the functionality
    of dictionary generation.

--- dictionary.c
+++ dictionary.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+#include <err.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,18 +11,32 @@ typedef struct word_count {
     int value;
 } count;
 
+static char *
+print_count(void *data)
+{
+    count *wc = (count *) data;
+    char *s;
+    asprintf(&s, "%d", wc->value);
+    return s;
+}
+
+static void
+free_count(void *data)
+{
+    count *c = (count *) data;
+    free(c);
+}
+
 int
 parse_file(const char *filename, spell_hashtable *table)
 {
     FILE *file = fopen(filename, "r");
     int _errno;
-    char *line = NULL;
     size_t len = 0;
     ssize_t read;
-    char *word;
+    char *word = NULL;
     count *c;
-    count default_count;
-    default_count.value = 1;
+    count *default_count;
 
     if (file == NULL) {
         _errno = errno;
@@ -28,18 +44,35 @@ parse_file(const char *filename, spell_hashtable *table)
         return errno;
     }
 
-    while ((read = getline(&line, &len, file)) != -1) {
-        while ((word = strchr(line, ' ')) != NULL) {
-            c = (count *) spell_hashtable_get(table, word);
-            if (c == NULL) {
-                spell_hashtable_add(table, word, &default_count);
-            } else {
-                c->value++;
-                spell_hashtable_add(table, word, c);
-            }
-            free(line);
+    while ((read = getdelim(&word, &len, ' ', file)) != -1) {
+        // TODO Use a more sophisitcated tokenization,
+        // perhaps preprocess with a stemmer algorithm first
+        word[read - 1] = 0;
+        c = (count *) spell_hashtable_get(table, word);
+        if (c == NULL) {
+            default_count = malloc(sizeof(count));
+            default_count->value = 1;
+            spell_hashtable_add(table, word, default_count);
+        } else {
+            c->value++;
+            spell_hashtable_add(table, word, c);
         }
     }
+    free(word);
     fclose(file);
     return 0;
 }
+
+int
+main(int argc, char **argv)
+{
+    spell_hashtable *table = spell_hashtable_init(10);
+    if (table == NULL) {
+        errx(EXIT_FAILURE, "Failed to initialize the hash table");
+    }
+    parse_file("file.txt", table);
+    spell_hashtable_dump(table, "dictionary.dump", print_count);
+    spell_hashtable_free(table, free_count);
+
+}
+
