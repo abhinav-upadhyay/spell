9b1493743ae9a3efd761d7e4a33722a38bf17538
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2014-07-27 19:33:01 +0530

    Properly extract the words from the lines in the file.

--- dictionary.c
+++ dictionary.c
@@ -44,7 +44,9 @@ sanitize_string(char *s)
     char *ret = malloc(strlen(s) + 1);
     int i = 0;
     while(*s) {
-        if (*s == ' ' || *s == '\t' || *s == '"' || *s == '\'' || *s == '\n' || *s == ',' || *s == '.' || *s == ';' || *s == ':') {
+        if (*s == ' ' || *s == '\t' || *s == '"' || *s == '\'' ||
+                *s == ',' || *s == '.' || *s == ';' ||
+                *s == ':') {
             s++;
             continue;
         }
@@ -71,8 +73,11 @@ parse_file(const char *filename, spell_hashtable *table)
     FILE *file = fopen(filename, "r");
     int _errno;
     size_t len = 0;
+    size_t wordlen;
     ssize_t read;
     char *word = NULL;
+    char *line = NULL;
+    char *templine;
     count *c;
     count *default_count;
 
@@ -82,27 +87,35 @@ parse_file(const char *filename, spell_hashtable *table)
         return errno;
     }
 
-    while ((read = getdelim(&word, &len, ' ', file)) != -1) {
+    while ((read = getline(&line, &len, file)) != -1) {
         // TODO Use a more sophisitcated tokenization,
         // perhaps preprocess with a stemmer algorithm first
-        word[read - 1] = 0;
-        to_lower(word);
-        char *sanitized_word = sanitize_string(word);
-        if (sanitized_word == NULL || strlen(sanitized_word) == 0) {
+        line[read - 1] = 0;
+        templine = line;
+        while (*templine) {
+            wordlen = strcspn(templine, " ");
+            templine[wordlen] = 0;
+            word = templine;
+            to_lower(word);
+            char *sanitized_word = sanitize_string(word);
+            if (sanitized_word == NULL || strlen(sanitized_word) == 0) {
+                free(sanitized_word);
+                templine += wordlen + 1;
+                continue;
+            }
+            c = (count *) spell_hashtable_get(table, sanitized_word);
+            if (c == NULL) {
+                default_count = malloc(sizeof(count));
+                default_count->value = 1;
+                spell_hashtable_add(table, sanitized_word, default_count);
+            } else {
+                c->value++;
+            }
             free(sanitized_word);
-            continue;
+            templine += wordlen + 1;
         }
-        c = (count *) spell_hashtable_get(table, sanitized_word);
-        if (c == NULL) {
-            default_count = malloc(sizeof(count));
-            default_count->value = 1;
-            spell_hashtable_add(table, sanitized_word, default_count);
-        } else {
-            c->value++;
-        }
-        free(sanitized_word);
     }
-    free(word);
+    free(line);
     fclose(file);
     return 0;
 }
