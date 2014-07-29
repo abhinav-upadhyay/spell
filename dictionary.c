daf8e7d7856d29f02536943bee145525ba1d0ab3
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2014-07-29 20:26:20 +0530

    Avoid parsing word beyond apostrophe.
    Avoid parsing abbreviations and URLs.
    Do not convert the original word to lower case, instead first call
    sanitize_string and afterword convert case.

--- dictionary.c
+++ dictionary.c
@@ -40,18 +40,69 @@ to_lower(char *s)
 {
     char c;
     char *t = s;
+    if (s == NULL) {
+        return;
+    }
     for (int i = 0; i < strlen(s); i++) {
         c = s[i];
         s[i] = tolower(c);
     }
 }
 
+static int
+is_upper(char *s)
+{
+    while (*s) {
+        if (isupper(*s++)) {
+            continue;
+        }
+        /* not uppercase string */
+        return 0;
+    }
+    /* uppercase string */
+    return 1;
+}
+
+
 static char *
 sanitize_string(char *s)
 {
-    char *ret = malloc(strlen(s) + 1);
+    size_t len = strlen(s);
+    char *ret = malloc(len + 1);
     int i = 0;
+    if (s[0] == '(' && s[len - 1] == ')') {
+        s[len - 1] = 0;
+        if (is_upper(s + 1)) {
+            return NULL;
+        }
+    }
+
+    if (is_upper(s)) {
+        return NULL;
+    }
+
     while(*s) {
+        /*
+         * Detect apostrophe and stop copying characters immediately
+         */
+        if ((*s == '\'') && (
+                 !strncmp(s + 1, "s", 1) ||
+                 !strncmp(s + 1, "es", 2) ||
+                 !strncmp(s + 1, "m", 1) ||
+                 !strncmp(s + 1, "d", 1) ||
+                 !strncmp(s + 1, "ll", 2))) {
+            break;
+        }
+
+        /*
+         * If the word contains a dot in between that suggests it is either
+         * an abbreviation or somekind of a URL. Do not bother with such words.
+         */
+        if (*s == '.') {
+            free(ret);
+            return NULL;
+        }
+
         if (!isalpha(*s)) {
             s++;
             continue;
@@ -103,12 +154,15 @@ parse_file(const char *filename, spell_hashtable *table)
             templine[wordlen] = 0;
             word = templine;
             templine += wordlen + 1;
-            to_lower(word);
             char *sanitized_word = sanitize_string(word);
-            if (sanitized_word == NULL || strlen(sanitized_word) == 0) {
+            if (sanitized_word == NULL) {
+                continue;
+            }
+            if (strlen(sanitized_word) == 0) {
                 free(sanitized_word);
                 continue;
             }
+            to_lower(sanitized_word);
             c = (count *) spell_hashtable_get(table, sanitized_word);
             if (c == NULL) {
                 default_count = malloc(sizeof(count));
